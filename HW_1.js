aaf5100d1ddd746b727c3efc5925f2f254a2a088
Iryna Malovana <malmall@gmail.com>
2023-03-12 19:24:12 +0200

    added HW_1

---
+++ HW_1.js
@@ -0,0 +1,39 @@
+'use strict'
+
+//1.Повернути негативний
+function makeNegative(num) {
+    if (num >= 0) {
+      return -num;
+    } else {
+      return num;
+    }
+  }
+  console.log(makeNegative(-9));
+
+//2.Протилежне число
+let num = 6;
+let oppositeNum = -num;
+console.log(oppositeNum); 
+ 
+//3.Парні чи непарнi
+function evenOrOdd(num) {
+    if(num % 2 === 0) {
+      return 'Even';
+    } else {
+      return 'Odd';
+    }
+  }
+  console.log(evenOrOdd(10));
+
+//4.Перетворіть число на рядок
+function numToString(num) {
+    return num.toString();
+  }
+  console.log(numToString(-10));
+
+//5.Перетворення логічних значень на рядки «Так» або «Ні»
+function BoolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+  }
+  console.log(BoolToWord(9>8));
+  
