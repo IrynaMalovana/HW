dffb5f84ce5e54409050519920e699fcd0933445
Iryna Malovana <malmall@gmail.com>
2023-03-14 18:38:31 +0200

    changed after feedback

--- HW_1.js
+++ HW_1.js
@@ -3,38 +3,38 @@
 
 //1.Повернути негативний
 function makeNegative(num) {
-    if (num >= 0) {
-      return -num;
-    } else {
-      return num;
-    }
-  }
+  return num <= 0 ? num : -num;
+};
   console.log(makeNegative(-9));
 
 //2.Протилежне число
-const num = 6;
-const oppositeNum = -num;
-console.log(oppositeNum); 
+function oppositeNum(number) {
+  return -number;
+}
+console.log(oppositeNum(9)); 
  
 //3.Парні чи непарнi
 function evenOrOdd(num) {
-    if(num % 2 === 0) {
-      return 'Even';
-    } else {
-      return 'Odd';
-    }
-  }
-  console.log(evenOrOdd(10));
+  return num % 2 === 0 ? 'Even' : 'Odd';
+ };
+ console.log(evenOrOdd(7));
 
 //4.Перетворіть число на рядок
 function numToString(num) {
     return num.toString();
-  }
+  };
   console.log(numToString(-10));
 
 //5.Перетворення логічних значень на рядки «Так» або «Ні»
-function BoolToWord(bool) {
+function boolToWord(bool) {
     return bool ? 'Yes' : 'No';
-  }
-  console.log(BoolToWord(9>8));
+  };
+  console.log(boolToWord(9>8));
+
+//6.Перевернуті рядки
+function reverseWord(word) {
+  return word.split('').reverse().join('');
+};
+console.log(reverseWord('world'));
+
   
